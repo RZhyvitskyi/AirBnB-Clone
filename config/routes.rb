0d06c2720f2893cbc0ded07d1bb6aac228b931b7
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-27 09:23:17 +0200

    devise fixed, pages created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  get "/home", to: "pages#home", as: "home"
+  get "/about", to: "pages#about", as: "about"
+
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  
 end
