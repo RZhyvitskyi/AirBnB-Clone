07de4d3c05a84b337cc5dc2b5eac7795b7c0e5be
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-27 11:06:17 +0200

    added booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_070916) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_27_085601) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.boolean "approved", default: false
+    t.bigint "user_id", null: false
+    t.bigint "demon_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["demon_id"], name: "index_bookings_on_demon_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "demons", force: :cascade do |t|
+    t.string "name"
+    t.string "speciality"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_070916) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "demons"
+  add_foreign_key "bookings", "users"
 end
