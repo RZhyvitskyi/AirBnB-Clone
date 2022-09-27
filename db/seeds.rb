0f8bdea1e4e9d27027a80dcc6ba95bee68f1082a
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-27 13:39:13 +0200

    seeding done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+
+User.destroy_all
+
+10.times do |i|
+  user = User.create(name: Faker::Movies::Lebowski.actor, 
+              email: Faker::Internet.email,
+              password: '654321')
+  5.times do |j|
+    demon = Demon.create(name: Faker::Fantasy::Tolkien.character,
+                       speciality: Faker::Fantasy::Tolkien.race,
+                       location: Faker::Fantasy::Tolkien.location,
+                       user_id: user.id)
+  end
+end
+
+puts 'Finished!'
