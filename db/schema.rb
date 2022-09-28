e63501b3dc9b4721baa94c0c38682d4a0399f75a
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-28 10:08:21 +0200

    add price to demon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_110029) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_28_075849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_110029) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "picture_url"
+    t.decimal "price", precision: 5, scale: 2
     t.index ["user_id"], name: "index_demons_on_user_id"
   end
 
