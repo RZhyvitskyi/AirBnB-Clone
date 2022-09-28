6aae2f3d9c6ff41e9c225fc3b80a67f45e426e6b
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-28 08:29:51 +0200

    add methods and routes to demons controller

--- app/controllers/demons_controller.rb
+++ app/controllers/demons_controller.rb
@@ -1,10 +1,20 @@
 class DemonsController < ApplicationController
+  before_action :get_demons, only: [:index, :new]
+
   def index
   end
 
   def show
+    @demon = Demon.find(params[:id])
   end
 
   def new
+    @demon = Demon.new
+  end
+
+  private
+
+  def get_demons
+    @demons = Demon.all
   end
 end
