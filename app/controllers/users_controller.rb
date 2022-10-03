69cb8a88f1e9159be4c9a7109c5f1f4162de595d
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-10-03 12:23:34 +0200

    add user edit role page and method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'Demon was successfully created.'
+    else
+      redirect_to users_path, alert: 'Demon was not created.'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(role_ids: [])
+  end
 end
--- spec/controllers/bookings_controller_spec.rb
+++ spec/controllers/bookings_controller_spec.rb
@@ -39,4 +39,6 @@ RSpec.describe BookingsController, type: :controller do
   end
 
   
+
+  
 end
