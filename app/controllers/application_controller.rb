9e1c5c14fb3898a40d2308d3bbbf8d8f7ccfb671
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-10-04 08:46:16 +0200

    add policy to users role administration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_user_params, if: :devise_controller?
 
-  include Pundit
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back(fallback_location: root_path)
+  end
 
   def configure_permitted_user_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
